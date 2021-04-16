3498f150f6063b5d8f7148ab1875016af627b1c1
rohansaxena02 <rohansaxena02@gmail.com>
2021-04-16 11:33:03 +0530

    app.js helpful comments added

--- app.js
+++ app.js
@@ -11,8 +11,11 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
+// parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true })); 
-app.use(express.json())
+// parse requests of content-type - application/json
+app.use(bodyParser.json())
+
 app.post('/save-data', function(req, res){
     console.log('Printing Response');
     x = req.body;
@@ -21,7 +24,7 @@ app.post('/save-data', function(req, res){
     console.log('This is the Value : '+ x['value']);
 });
 
-
+// listen for requests
 app.listen(PORT, function () {
     console.log(`Ready on port ${PORT}!`)
 })
